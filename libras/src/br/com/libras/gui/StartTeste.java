d8d9ba5bd8118f9d56e381e95a6ea0d8161cc840
wev <wev@wev-Console>
2017-07-31 08:14:25 -0300

    Modificações na classe FImagens

--- libras/src/br/com/libras/gui/StartTeste.java
+++ libras/src/br/com/libras/gui/StartTeste.java
@@ -3,6 +3,10 @@ package br.com.libras.gui;
 
 public class StartTeste {
 
-	
+	public static void main(String[] args) {
+		FImagens fImagens = new FImagens();
+		fImagens.setVisible(true);
+		
+	}
 }
 
