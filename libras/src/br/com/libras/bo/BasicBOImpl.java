8bc971d55f38e27055e595479b140487ec2d81af
wevkleyton <kleyton.cpd@gmail.com>
2017-08-08 13:40:36 +0000

    Criação de classe para comparar arquivos

--- libras/src/br/com/libras/bo/BasicBOImpl.java
+++ libras/src/br/com/libras/bo/BasicBOImpl.java
@@ -1,12 +1,5 @@
 package br.com.libras.bo;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.net.ConnectException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class BasicBOImpl {
     
---
+++ libras/src/br/com/libras/teste/ComparaArquivos.java
@@ -0,0 +1,25 @@
+package br.com.libras.teste;
+
+import java.io.File;
+
+
+public class ComparaArquivos {
+
+    public static void verifica(String arq1, String arq2) {
+
+        File arquivo1 = new File(arq1);
+        File arquivo2 = new File(arq2);
+        int byte_arquivo1;
+        int byte_arquivo2;
+
+        if (arquivo1.length() == arquivo2.length()) {
+          try {
+                
+        } catch (Exception e) {
+            // TODO: handle exception
+        }  
+        }
+    }
+
+
+}
