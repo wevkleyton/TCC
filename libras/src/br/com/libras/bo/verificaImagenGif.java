843884f80f72ff6aafd02824aea473977457760e
wev <kleyton.cpd@gmail.com>
2017-08-11 18:23:17 -0300

    teste de commit idea classe verificaimagens

--- libras/src/br/com/libras/bo/verificaImagenGif.java
+++ libras/src/br/com/libras/bo/verificaImagenGif.java
@@ -6,16 +6,19 @@ import javax.swing.JFileChooser;
 
 public class verificaImagenGif {
 
-	public verificaImagenGif(){
+	public File verificaImagen(){
+		File file = null;
 		JFileChooser jFileChooser = new JFileChooser();
 		int result = jFileChooser.showOpenDialog(null);
 		if (result == JFileChooser.APPROVE_OPTION){
-			File file = jFileChooser.getSelectedFile();
+			file = jFileChooser.getSelectedFile();
 			System.out.println(file.getAbsolutePath());
 		}
+		return  file;
 	}
 
 	public static void main (String Args[]){
+
 
 	}
 }
