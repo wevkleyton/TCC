467d26efa6446c977d7bca7e7ed10ffb4a6ecec1
kleyton.cpd@gmail.com <wkmo11985pa>
2017-08-28 19:41:36 -0300

    inclusão de bean de video, e libs

--- libras/src/br/com/libras/bean/Video.java
+++ libras/src/br/com/libras/bean/Video.java
@@ -1,5 +1,24 @@
 package br.com.libras.bean;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.lang.annotation.Retention;
+
+/**
+ * Autor: Wev Kleyton 27/08/2017
+ * Criação do Bean de Video
+ */
+
+@Entity
 public class Video {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String nome;
+
 
 }
--- libras/src/br/com/libras/gui/StartApp.java
+++ libras/src/br/com/libras/gui/StartApp.java
@@ -4,9 +4,7 @@ package br.com.libras.gui;
 public class StartApp {
 
 	public static void main(String[] args) {
-		FImagens fImagens = new FImagens();
-		fImagens.setVisible(true);
-		
+
 	}
 }
 
