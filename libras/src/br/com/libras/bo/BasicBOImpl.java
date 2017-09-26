6e7d44d13a3274917164b948af338cc8ece3c823
wev <kleyton.cpd@gmail.com>
2017-09-26 19:02:41 -0300

    Correção de erros no BasicoBOImpl e TesteDeCodigos

--- libras/src/br/com/libras/bo/BasicBOImpl.java
+++ libras/src/br/com/libras/bo/BasicBOImpl.java
@@ -4,9 +4,5 @@ package br.com.libras.bo;
 public class BasicBOImpl {
     
 
-	@SuppressWarnings("unused")
-	private void salvaImagen(){
-		verificaImagenGif verifica = new verificaImagenGif();
-	}
 
 }
--- libras/src/br/com/libras/teste/testeDecodigos.java
+++ libras/src/br/com/libras/teste/testeDecodigos.java
@@ -8,11 +8,7 @@ public class testeDecodigos {
         int resultado = JOptionPane.showConfirmDialog(null, "Deseja relamente cancelar ?",
                 "teste",JOptionPane.OK_CANCEL_OPTION);
 
-        System.out.println(resultado == 0 ? true : false"teste de commit2");
-        System.out.println(resultado == 0 ? true : false"teste de commit3");
-        System.out.println(resultado == 0 ? true : false"teste de commit4");
-        System.out.println(resultado == 0 ? true : false"teste de commit5");
-        
+           
         
     }
 }
