b5f49e8613d33c276d17910319170248941ec5ba
wev <kleyton.cpd@gmail.com>
2017-09-28 18:49:15 -0300

    Modificações no BasicBOImpl

--- libras/src/br/com/libras/bo/BasicBOImpl.java
+++ libras/src/br/com/libras/bo/BasicBOImpl.java
@@ -1,8 +1,22 @@
 package br.com.libras.bo;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.channels.FileLockInterruptionException;
 
 public class BasicBOImpl {
     
 
+	public void updateImagem() {
+		
+		try {
+			File file = new File("");
+			FileInputStream fileInputStream = new FileInputStream(file);
+//			update mytable set avatarImg = pg_read_file('/Users/myUser/profile.png')::bytea where userid=5;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
