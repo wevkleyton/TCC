346480a281f5564eb4403ea4c8e610f8c6cfaa8e
wev <kleyton.cpd@gmail.com>
2017-09-29 19:01:49 -0300

    Modificações nos aruqivo query_sequence.sql e na classe BasicBOImpl

--- libras/src/br/com/libras/bo/BasicBOImpl.java
+++ libras/src/br/com/libras/bo/BasicBOImpl.java
@@ -1,22 +1,49 @@
 package br.com.libras.bo;
 
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.nio.channels.FileLockInterruptionException;
+import java.sql.PreparedStatement;
+
+import javax.swing.JFileChooser;
+
+import br.com.libras.dao.ConectaJDBC;
 
 public class BasicBOImpl {
     
 
 	public void updateImagem() {
 		
+		
 		try {
-			File file = new File("");
-			FileInputStream fileInputStream = new FileInputStream(file);
+			
+			JFileChooser jFileChooser = new JFileChooser();
+			jFileChooser.setFileSelectionMode(jFileChooser.FILES_ONLY);
+			int retorno = jFileChooser.showOpenDialog(null);	
+			File file = jFileChooser.getSelectedFile();
+			
+			byte[] imagem = new byte[(int) file.length()];
+			System.out.println("lendo imagem" + imagem.length + "bytes...");
+			DataInputStream dataInputStream = new DataInputStream(new FileInputStream(file));
+			dataInputStream.readFully(imagem);
+			dataInputStream.close();
+			ConectaJDBC.Conecta("insert into imagem values (1,)");
+			
+			
+
+			
+//			System.out.println(file.getAbsolutePath());
 //			update mytable set avatarImg = pg_read_file('/Users/myUser/profile.png')::bytea where userid=5;
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void main(String[] args) {
+		BasicBOImpl basicBOImpl = new BasicBOImpl();
+		basicBOImpl.updateImagem();
 	}
 
 }
--- libras/src/sqls/query_sequence.sql
+++ libras/src/sqls/query_sequence.sql
@@ -1,15 +1,17 @@
-insert into usuario (id,nome, senha,tipo) values(1,'kleyton','123456','A')
-
-select * from usuario where nome = 'kleyton'
+/* Seleção de usuario*/
+select * from usuario
+/* Inclusão de usuario*/
+insert into usuario values(next,'wev','123456','A')
+/* Update de usuario*/
 update usuario set senha = '7c4a8d09ca3762af61e59520943dc26494f8941b' where id = 1
-
-
+/* Deleta Todos os usuarios*/
 delete from usuario
 
+/* Altera coluna*/
 alter table usuario  alter column senha type character varying(100)
 
 -- Cria auto encremento 
-CREATE SEQUENCE lusuario_id_seq
+CREATE SEQUENCE imagem_id_seq
  INCREMENT 1
  MINVALUE 1
  MAXVALUE 9223372036854775807
@@ -24,4 +26,12 @@ ALTER SEQUENCE libras.usuario_id_seq
  RESTART 1
  CACHE 1;
   
-ALTER TABLE usuario ALTER COLUMN id SET DEFAULT NEXTVAL('usuario_id_seq') ;
+ALTER TABLE imagem ALTER COLUMN id SET DEFAULT NEXTVAL('imagem_id_seq') ;
+
+
+
+
+select * from imagem
+
+INSERT INTO imagem (id,descricao,imagem) VALUES (1,'jasper',lo_import('/tmp/wev.jpg'));
+select lo_export('/tmp/wev.jpg') from imagem where descricao = 'jasper'
